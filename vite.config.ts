d85469a4444e2c51686c39482198c44799018dfa
AbdelaliAAZZ <abdelaliaazzaoui@gmail.com>
2025-03-28 00:36:52 +0000

    thirth attempt to fix the errors to deploy in vercle

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
   base: './',
+  build: {
+    
+    minify: true,
+    sourcemap: false,
+  },
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), './src'),
